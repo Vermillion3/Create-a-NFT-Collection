d609c35cde86a52feda7f5cefcaa8b5162aaecef
JoseDometita <104407442+Vermillion3@users.noreply.github.com>
2023-07-27 10:06:16 +0700

    script.js

---
+++ script.js
@@ -0,0 +1,34 @@
+const NFTS = []
+
+function mintNFT(Name, Nationality, Gender, accessories){
+    const NFT = {
+      name: Name,
+      country: Nationality,
+      Gender: Gender,
+      accessories: accessories,
+      
+    }
+    console.log("Minted: " + Name)
+    NFTS.push(NFT);
+}
+
+
+function listNFTS(){
+    for(let i = 0; i < NFTS.length; i++){
+        console.table(NFTS[i])
+    }
+}
+
+function getTotalSupply(){
+    console.log("Total NFT's: " + NFTS.length)
+}
+
+mintNFT("Jose", "Filipino", "Male", "Gold Watch" );
+mintNFT("Jay", "British", "Male", "Silver Belt" );
+mintNFT("john", "Canadian", "Male", "Sun Glasses" );
+mintNFT("Kay", "Spanish", "Female", "Pair of Earings" );
+
+
+
+listNFTS();
+getTotalSupply()
